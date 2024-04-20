e726f7e254ae86afb83671bbde77806c4af8055c
Di <didoesdigital+git@gmail.com>
2024-04-20 18:39:11 +1000

    Generate Tailwind classes for blog and MDX content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/blog/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/mdx-components.tsx",
   ],
   theme: {
     fontFamily: {
