47756fb52caa0d99b5f6bf356340b4c32a5c6ddd
Di <didoesdigital+git@gmail.com>
2024-04-20 15:03:31 +1000

    Configure Next.js to use MDX

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
