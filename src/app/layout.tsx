ea394a77f64f0b5fc11507fcefc3f32c22116ebb
Di <didoesdigital+git@gmail.com>
2024-04-21 17:59:01 +1000

    Add site header and nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { crimson, workSans, overpassMono } from "@/app/_components/fonts/fonts";
 import type { Metadata } from "next";
 import "./globals.css";
@@ -17,7 +18,25 @@ export default function RootLayout({
       lang="en"
       className={`${crimson.variable} ${workSans.variable} ${overpassMono.variable} font-serif text-neutral-800`}
     >
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav className="container mx-auto mt-12 flex max-w-screen-lg flex-wrap justify-between gap-y-2 px-5">
+            <div className="prose prose-xl">
+              <Link href="/" className="text-2xl font-semibold tracking-wide">
+                My site name
+              </Link>
+            </div>
+            <div className="prose prose-xl flex flex-wrap gap-x-4 gap-y-0">
+              <p>
+                <Link className="font-sans tracking-wide" href="/blog">
+                  Blog
+                </Link>
+              </p>
+            </div>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
