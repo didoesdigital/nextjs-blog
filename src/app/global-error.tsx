6fd8fe20e6c1f8876fa4af436ffac818fafa15d2
Di <didoesdigital+git@gmail.com>
2024-04-28 20:49:15 +1000

    Expand global-error page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -7,11 +7,34 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  console.error(error);
+
   return (
-    <html>
+    <html
+      lang="en"
+      className="bg-white font-serif text-neutral-800 dark:bg-neutral-950"
+    >
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <div className="prose mx-auto flex min-h-screen flex-col items-center justify-center text-center dark:prose-invert">
+          <h1 className="mr-4 inline-block pr-4 text-4xl font-semibold">
+            Sorry, something went wrong.
+          </h1>
+          <div className="inline-block">
+            <p className="text-md">
+              <button
+                className="font-semibold text-teal-700"
+                onClick={() => reset()}
+              >
+                Try again
+              </button>{" "}
+              or{" "}
+              <a className="tracking-wide text-teal-700" href="/">
+                go home
+              </a>
+              .
+            </p>
+          </div>
+        </div>
       </body>
     </html>
   );
