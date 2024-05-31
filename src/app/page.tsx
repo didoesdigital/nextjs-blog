2ebc913b5aaabcf6022e41ee1bf35cf040bba27e
Di <didoesdigital+git@gmail.com>
2024-05-31 22:59:56 +1000

    Update homepage layout, add SITE_DESCRIPTION, and…
    … remove unused image file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,20 @@
+import { SITE_DESCRIPTION } from "@/app/lib/constants/site";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="border-gray-300 from-zinc-200 lg:bg-gray-200 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30 fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl lg:static  lg:w-auto lg:rounded-xl lg:border lg:p-4 dark:border-neutral-800">
+      <div className="prose prose-xl z-10 w-full max-w-5xl items-center dark:prose-invert lg:flex">
+        <p className="flex w-full justify-center border-b">
+          {SITE_DESCRIPTION}
+        </p>
+      </div>
+
+      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <p className="border-gray-300 from-zinc-200 lg:bg-gray-200 dark:bg-zinc-800/30 dark:from-inherit lg:dark:bg-zinc-800/30 prose fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b pb-6 pt-8 backdrop-blur-2xl dark:prose-invert lg:static  lg:w-auto lg:rounded-xl lg:border lg:p-4 dark:border-neutral-800">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:size-auto lg:bg-none dark:from-black dark:via-black">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
       <div className="after:from-sky-200 after:dark:from-sky-900 relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:via-blue-200 after:blur-2xl after:content-[''] sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:via-[#0141ff] after:dark:opacity-40">
@@ -39,7 +28,7 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="prose prose-xl mb-32 grid text-center dark:prose-invert lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="hover:border-gray-300 hover:bg-gray-100 hover:dark:bg-neutral-800/30 group rounded-lg border border-transparent px-5 py-4 transition-colors hover:dark:border-neutral-700"
